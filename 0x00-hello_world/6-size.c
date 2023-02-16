c3d1d8df36210c596eb2a646a4784a513fb647b9
NhloniNkwinika <hloninkwinika@gmail.com>
2023-02-16 11:13:35 -0800

    sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio>
+/**
+ * main - this program prints the varios types on the computer it is compiled and run on
+ * Return: 0 (Sucess)
+ */
+
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
+}	
