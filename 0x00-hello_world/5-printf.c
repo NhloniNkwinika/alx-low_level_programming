a2875bb33a1783654a33630a299e9ca3eaf1a511
NhloniNkwinika <hloninkwinika@gmail.com>
2023-02-16 05:16:25 -0800

    printf fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - this program prints a string using the printf function
- * return: 0 (Success)
+ * Return: 0 (Success)
  */
 
 int main(void)
 {
-printf("with proper grammer, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
