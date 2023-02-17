d612983b01fcfca869882c65b2ee6f778e1230d6
NhloniNkwinika <hloninkwinika@gmail.com>
2023-02-17 08:20:25 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+
+/**
+ * main - prints all possible different combinations of two digits
+ *
+ * Return: 0
+ */
+int main(void)
+{
+        int i, j;
+
+        i = 0;
+        while (i < 10)
+        {
+                j = i + 1;
+                while (j < 10)
+                {
+                        putchar(i + '0');
+                        putchar(j + '0');
+                        if (i < 8)
+                        {
+                                putchar(',');
+                                putchar(' ');
+                        }
+                        j++;
+                }
+                i++;
+        }
+        putchar('\n');
+        return (0);
+}
+
