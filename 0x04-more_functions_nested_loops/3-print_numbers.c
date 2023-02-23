2761b10fb2f5e85769e7d83cf24ef67d6bb9901b
NhloniNkwinika <hloninkwinika@gmail.com>
2023-02-23 11:01:32 -0800

    numbers fix

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 /**
  * print_numbers - prints numbers between 0 to 9.
  * Return: no return.
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,8 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_most_numbers - Prints the numbers from 0-9 except for 2 and 4.
+ * Return: void
  */
 void print_most_numbers(void)
 {
@@ -8,7 +10,7 @@ int num;
 for (num = 0; num <= 9; num++)
 {
 if (num != 2 && num != 4)
-_putchar((num % 10) + '0');
+putchar((num % 10) + '0');
 }
-_putchar('\n');
+putchar('\n');
 }
